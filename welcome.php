423f5d9014a123af4a8581b483cffa4038658a74
Vinicios Augusto <scrapbonnie4@gmail.com>
2025-08-30 02:24:53 +0000

    atc 2

--- welcome.php
+++ welcome.php
@@ -1,7 +1,13 @@
+<!DOCTYPE html>
 <html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Welcome</title>
+    </head>
     <body>
         Welcome <?php echo $_POST["name"]; ?>
         <br>
         Your email address is: <?php echo $_POST["email"]; ?>
+        <br>
     </body>
 </html>
